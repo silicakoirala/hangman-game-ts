cfee6db25fdeb66d30c51808016c82e33308811c
Silica Koirala <silicakoirala@gmail.com>
2024-05-09 13:18:46 +0545

    feat(hangmandrawing, hangmanword, keyboard):Add HangmanDrawing, HangmanWord, Keyboardcomponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react"
+import words from "./data/wordList.json"
+import HangmanWord from "./components/HangmanWord"
+import HangManDrawing from "./components/HangManDrawing"
+import Keyboard from "./components/Keyboard"
+
 
 function App() {
+  const [wordToGuess, setWordToreturnGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
+  console.log(wordToGuess)
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className=" flex flex-col max-w-[800px] gap-4 mx-auto my-0 align-items-center">
+      <div className="font-xl text-center">
+        Lose
+        Win 
+      </div>
+      <HangManDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
   )
 }
 
